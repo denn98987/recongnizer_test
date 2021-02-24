ac2a292daa2b5b3d35329de9e1aaf4ce7611e1c0
denn98987 <denn98987@gmail.com>
2021-02-24 22:12:35 +0500

    Replace class component with functional component

--- components/PredictCard.js
+++ components/PredictCard.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import { Image, View, StyleSheet, StatusBar, Text } from "react-native";
 import * as tf from "@tensorflow/tfjs";
 import * as mobilenet from "@tensorflow-models/mobilenet";
@@ -17,62 +17,54 @@ const Item = ({ item }) => (
   </View>
 );
 
-class PredictCard extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      prediction: [],
-      isTfReady: false,
+const PredictCard = ({ route }) => {
+  const [prediction, setPrediction] = useState([]);
+  const isTfReady = useState(false);
+
+  useEffect(() => {
+    const processingPrediction = async () => {
+      await tf.setBackend(BACKEND_TO_USE);
+      await tf.ready();
+      // Load mobilenet
+      const model = await mobilenet.load();
+
+      //warmup mobilenet
+      await model.classify(tf.zeros([1, 224, 224, 3]));
+
+      const rawImageData = await preprocessImage(route.params.uri);
+      const imageTensor = imageToTensor(rawImageData);
+      const imageTensorSum = imageTensor.sum();
+
+      setPrediction(await model.classify(imageTensor));
+      tf.dispose([imageTensor, imageTensorSum]);
     };
-  }
+    processingPrediction();
+  }, []);
 
-  async componentDidMount() {
-    await tf.setBackend(BACKEND_TO_USE);
-    await tf.ready();
-    // Load mobilenet
-    this.model = await mobilenet.load();
-
-    //warmup mobilenet
-    await this.model.classify(tf.zeros([1, 224, 224, 3]));
-
-    const rawImageData = await preprocessImage(this.props.route.params.uri);
-    const imageTensor = imageToTensor(rawImageData);
-    const imageTensorSum = imageTensor.sum();
-
-    const prediction = await this.model.classify(imageTensor);
-
-    this.setState({ prediction });
-    tf.dispose([imageTensor, imageTensorSum]);
-  }
-
-  render() {
-    const { prediction } = this.state;
-    const renderItem = ({ item }) => {
-      return <Item item={item} />;
-    };
-
-    return (
-      <View style={styles.sectionContainer}>
-        <Image
-          style={styles.cardImg}
-          source={{
-            uri: this.props.route.params.uri,
-          }}
-        ></Image>
-        <Text style={styles.resultTextHeader}>Results</Text>
-        {prediction.length === 0 ? (
-          <DotsLoader style={styles.loader} />
-        ) : (
-          <FlatList
-            data={prediction}
-            renderItem={renderItem}
-            style={styles.list}
-          ></FlatList>
-        )}
-      </View>
-    );
-  }
-}
+  const renderItem = ({ item }) => {
+    return <Item item={item} />;
+  };
+  return (
+    <View style={styles.sectionContainer}>
+      <Image
+        style={styles.cardImg}
+        source={{
+          uri: route.params.uri,
+        }}
+      ></Image>
+      <Text style={styles.resultTextHeader}>Results</Text>
+      {prediction.length === 0 ? (
+        <DotsLoader style={styles.loader} />
+      ) : (
+        <FlatList
+          data={prediction}
+          renderItem={renderItem}
+          style={styles.list}
+        ></FlatList>
+      )}
+    </View>
+  );
+};
 
 export default PredictCard;
 
